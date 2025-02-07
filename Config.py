5417edb023d73f64539fa22b9dfbb96c98ec8451
GeorgesMoulaa <pierre.iung01@gmail.com>
2025-02-07 14:40:36 +0100

    Delete Config.py

--- Config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN", "8183061202:AAEGqmjBUB6owUjGs6KoJxxnb...")
-TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID", "949838495")
-MEXC_API_URL = os.getenv("MEXC_API_URL", "https://www.mexc.com")
